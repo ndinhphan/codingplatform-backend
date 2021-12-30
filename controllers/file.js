8e7257616bc02512cf24f4363d6d92e91322548b
ndinhphan <48981913+ndinhphan@users.noreply.github.com>
2021-12-30 23:11:49 +0700

    nodemon ignore projects folder

--- controllers/file.js
+++ controllers/file.js
@@ -195,7 +195,7 @@ fileRouter.delete("/:id/file", middleware.userExtractor, async (req, res) => {
       .json({ message: "Cannot create file at the moment" });
   }
 });
-
+//update file content
 fileRouter.put("/:id/file", middleware.userExtractor, async (req, res) => {
   const { fileId, code } = req.body;
   const db = getDb();
@@ -278,6 +278,104 @@ fileRouter.put("/:id/file", middleware.userExtractor, async (req, res) => {
     }
 
     return res.status(200).json({ findFileById }).end();
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: "Cannot update file at the moment" });
+  }
+});
+
+//update multiple files content
+fileRouter.put("/:id/files", middleware.userExtractor, async (req, res) => {
+  const { files } = req.body;
+  const db = getDb();
+
+  try {
+    const project = await db.Project.findOne({
+      where: { id: req.params.id },
+      include: [
+        { model: db.User, attributes: ["id", "username", "email"] },
+        {
+          model: db.Folder,
+          as: "folders",
+          attributes: ["id", "name", "path", "folderId"],
+        },
+        { model: db.File, as: "files", attributes: ["id", "name", "folderId"] },
+      ],
+    });
+    if (!project) return res.status(404).json({ message: "Project not found" });
+
+    if (project.dataValues.User.dataValues.id !== req.user.dataValues.id) {
+      return res
+        .status(403)
+        .json({ message: "Cannot access unauthorized project!" });
+    }
+    if (!project) return res.status(404).json({ message: "Project not found" });
+
+    const updatedfiles = [];
+
+    files.forEach(async (file) => {
+      const { fileId, code } = file;
+      const findFileById = await db.File.findOne({
+        where: { id: fileId },
+      });
+
+      if (!findFileById) {
+        return res.status(404).json({ message: "file not found" });
+      }
+      let parentFolderPath = "";
+      if (findFileById.folderId) {
+        const parentFolder = await db.Folder.findOne({
+          where: { id: findFileById.folderId },
+        });
+        parentFolderPath = parentFolder.dataValues.path;
+      }
+
+      let projectfolders = project.dataValues.folders;
+      const foldermapping = {};
+      projectfolders.map((folder) => {
+        foldermapping[`${folder.id}`] = folder.name;
+      });
+      const folderpathmapping = {};
+      projectfolders = projectfolders.map((folder) => {
+        let folderpath = folder.dataValues.path;
+        // console.log(folder.dataValues.path);
+        for (folderid in foldermapping) {
+          let replace = new RegExp(`<${folderid}>`);
+          folderpath = folderpath.replace(replace, foldermapping[folderid]);
+        }
+        folder.dataValues.path = folderpath;
+        folderpathmapping[`${folder.id}`] = folderpath;
+        return {
+          ...folder,
+        };
+      });
+      // console.log(folderpathmapping);
+      let parentFolderPathInFs = "";
+      if (findFileById.dataValues.folderId)
+        parentFolderPathInFs =
+          folderpathmapping[findFileById.dataValues.folderId];
+
+      const filePathInFs = path.join(
+        project.dataValues.rootpath,
+        parentFolderPathInFs,
+        findFileById.dataValues.name
+      );
+      // console.log(filePathInFs);
+      try {
+        await updateFile({
+          filePath: filePathInFs,
+          content: code,
+        });
+      } catch (error) {
+        return res.status(409).json({ message: error.message }).end();
+      }
+      updatedfiles.push(findFileById.dataValues);
+    });
+
+    // console.log(updatedfiles);
+    return res.status(200).json({ files: "files are updated" }).end();
   } catch (error) {
     console.log(error);
     return res
